c4d9571de65aa5cdd8323ae3d0abd43c081abaf6
Rodion <klapan7x@gmail.com>
2022-08-08 11:36:57 +0300

    изменил в читабельный вид вывод ответа

--- quest2.php
+++ quest2.php
@@ -8,8 +8,9 @@ $quest3 = readline("Введите ТРЕТЬЮ задачу на сегодня
 $quest3Time = (int)readline("Сколько примерно времени эта задача займет?: ");
 $res = $quest1Time + $quest2Time + $quest3Time;
 
-echo "\n\n\n" . $name . " " . 'у вас запланировано 3 приоритетных задачи на день:' . "\n";
-echo "- $quest1 ({$quest1Time}ч)\n";
-echo "- $quest2 ({$quest2Time}ч)\n";
-echo "- $quest3 ({$quest3Time}ч)\n";
-echo 'Примерное время выполнения плана = ' . $res . 'ч';
+echo "{$name}, у вас запланировано 3 приоритетных задачи на день:
+- {$quest1} ({$quest1Time}ч)
+- {$quest2} ({$quest2Time}ч)
+- {$quest3} ({$quest3Time}ч)
+Примерное время выполнения плана = {$res}ч;
+";
