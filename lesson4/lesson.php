2944959fd4819a5ebe5852937f09b2c7f1f4e9ee
Rodion <klapan7x@gmail.com>
2022-08-16 08:38:44 +0300

    код с лекции и первое здание выполнил

---
+++ lesson4/lesson.php
@@ -0,0 +1,38 @@
+<?php
+
+// while (!$taskAmount = (int)readline("сколько задач запланировано на сегодня?")) {
+//   echo "Пожалуйста, введите корректное числовое значение";
+// }
+
+// $task = [];
+// for ($i = 1; $i <= $taskAmount; $i++) {
+//   while (!$taskText = readline("Опишите вашу $i задачу:")) {
+//     echo "Пожалуйста, введите описание для задачи";
+//   }
+//   while (!$time = (int)readline("Сколько времени она займет?")) {
+//     echo "Пожалуйста, введите корректное числовое значение";
+//   }
+
+//   $task[$taskText] = $time;
+// }
+
+// print_r($task);
+
+//   while (!$number = (int)readline($message)) {
+//     echo "Пожалуйста, введите корректное числовое значение\n";
+//   }
+
+//   return $number;
+// }
+
+// readNumber(54);
+
+// function readNonEmptyLine($message, $error = "Введите значение\n")
+// {
+//   while (!$line = readline($message)) {
+//     echo $error;
+//   }
+//   return $line;
+// }
+
+// $taskAmount = readNonEmptyLine("Сколько задач запланировано на сегодня?\n");
